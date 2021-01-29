390e52ddccb114c3e123fd131c37fabd9358df8c
PhamQuang-TocXanh <77691572+PhamQuang-TocXanh@users.noreply.github.com>
2021-01-29 14:05:59 +0700

    Phan

---
+++ A_Bai2.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+/*int main(){
+    int k;cin>>k;
+    for(;k>=3;k/=3);
+    cout<<k;
+}*/
+
+
+
+/*int main(){
+    int k;cin>>k;
+    while(k>=3) k/=3;
+    cout<<k;
+}*/
+
+
+
+
+int main(){
+    int k;cin>>k;
+    if(k>=3)
+    do{
+        k/=3;
+    }while(k>=3);
+    cout<<k;
+}
